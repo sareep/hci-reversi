c15371d729ab5fb991f81c2bcc31a5f9db893cdd
Sam Reep <sareep@westmont.edu>
2019-10-28 13:32:05 -0700

    added chat room functionality

--- public/js/main.js
+++ public/js/main.js
@@ -13,9 +13,57 @@ function getUrlParams(whichParam){
     }
 }
 
-var username = getUrlParams('username');
+var username = getUrlParams('username').split('%20').join('_');
 if('undefined' == typeof username || !username){
     username = "Anonymous_"+Math.random()*10;
 }
 
-$('#messages').append('<h4>'+username+'<h4>');
+var chat_room = 'Room_One';
+
+
+/* Connect to the socket server */
+var socket = io.connect();
+
+socket.on('log',function (array){
+    console.log.apply(console, array)
+});
+
+//TODO: This doesn't run when server.js emits to here.
+socket.on('join_room_response', function(payload){
+    console.log('join response: '+payload.username)
+    if(payload.result == 'fail'){
+        alert(payload.message);
+        return;
+    }
+    $('#messages').append('<p><b>New user has joined:</b> '+payload.username+'</p>');
+});
+
+socket.on('send_message_response', function(payload){
+    if(payload.result == 'fail'){
+        alert(payload.message);
+        return;
+    }
+    $('#messages').append('<p><b>'+payload.username+' says:</b> '+payload.message+'</p>');
+});
+
+function sendMessage(){
+    var payload = {};
+    payload.room = chat_room;
+    payload.username = username;
+    payload.message = $('#send_message_holder').val();
+    console.log('*** Client Log Message: \'send_message\' payload: '+JSON.stringify(payload));
+    socket.emit('send_message',payload)
+    
+    //blank the message holder after the message is posted
+    $("input[id=send_message_holder]").val('');
+}
+
+$(function(){
+    var payload = {};
+    payload.room = chat_room;
+    payload.username = username;
+
+    console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
+    socket.emit('join_room', payload);
+
+})
